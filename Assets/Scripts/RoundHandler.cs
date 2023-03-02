c8e4472f955be385d2ab965ddd38b5636a0b6d98
CalmEyE <alex.f.eriksson@gmail.com>
2023-03-02 11:07:06 +0100

    Credits Scene

---
+++ Assets/Scripts/RoundHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundHandler : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    
+}
